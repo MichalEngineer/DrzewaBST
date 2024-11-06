542ccca1c629873c97a42fbfeb90777ea8504a05
Michal <michal2000jd@gmail.com>
2024-11-06 23:40:17 +0100

    Komentarze cd.

--- DrzewaBST/BST.h
+++ DrzewaBST/BST.h
@@ -2,17 +2,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+/**
+ * @class BST
+ * @brief Klasa implementuj¹ca binarne drzewo poszukiwañ (BST).
+ */
 class BST {
 private:
+    /**
+    * @class Node
+    * @brief Wêze³ drzewa BST przechowuj¹cy wartoœæ oraz wskaŸniki do lewego i prawego dziecka.
+    */
     class Node {
     public:
-        int data;         
-        Node* left;       
-        Node* right;      
-        Node(int value) : data(value), left(nullptr), right(nullptr) {}
+        int data; // <--- Wartoœæ przechowywane w wêŸle
+        Node* left;  // <--- WskaŸnik na lewy korzen
+        Node* right;  // <--- WskaŸnik na prawy korzen
+        Node(int value) : data(value), left(nullptr), right(nullptr) {}  // <--- Konstruktor tworz¹cy wezel z podana wartoscia
     };
-    Node* root;           
+    Node* root; // <--- WskaŸnik na 
     void addNode(Node*& node, int value);
     Node* deleteNode(Node* node, int value);
     void deleteTree(Node* node);
