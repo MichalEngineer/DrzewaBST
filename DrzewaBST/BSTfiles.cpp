88f635ffd69c0369b41affa2b38870407c764a92
OG-AdiG <adriangargula@wp.pl>
2024-11-06 20:29:40 +0100

    Zapisywanie 0,1

--- DrzewaBST/BSTfiles.cpp
+++ DrzewaBST/BSTfiles.cpp
@@ -37,3 +37,20 @@ void Files::Save_to_text_file(std::ofstream& file, Node* node) {
     Pliki_Zapisanie_wezla(file, node->right);
 
 }
+
+void Files::Save_to_binary_file(BST& tree, const std::string& filename) {
+
+    std::ofstream file(filename, std::ios::binary);   
+
+    if (!file) {        
+
+        std::cerr << "Nie mo¿na otworzyæ pliku do zapisu: " << filename << std::endl;
+        return;
+
+    }
+
+    Pliki_Zapisanie_wezla(file, tree.root); 
+
+    file.close();
+
+}
